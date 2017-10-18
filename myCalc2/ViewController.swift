fd07486fdac106613c8abbb509fbb439f497375a
AMI <a_yamamoto@innovation.co.jp>
2017-10-18 19:29:53 +0900

    2017/10/18_Functionを作り始めた

--- myCalc2/ViewController.swift
+++ myCalc2/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //@は、予約語（これはインターフェービルダー）ここでパーツを宣言してる
+    @IBOutlet weak var priceField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapActionButton(_ sender: Any) {
+        let value = priceField.text! + "1"
+//        print(Int(value)) ←intでキャストするとオプショナル型
+        
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    //2のボタン
+    @IBAction func tap2Button(_ sender: Any) {
+        let value = priceField.text! + "2"
+        if let price = Int(value){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    
+    //0のボタン
+    @IBAction func tap0Button(_ sender: Any) {
+        let value = priceField.text! + "0"
+        if let price = Int(vlue){
+            priceField.text = "\(price)"
+        }
+    }
+    
+    
+    //Cをおした時
+    @IBAction func tupClearButton(_ sender: Any) {
+        priceField.text = "0"
+    }
+    
+    //宿題　全ての数字のボタンをFunction化してくる
+    
 }
 

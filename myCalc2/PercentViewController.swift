a60db926a35014095439b2166ee6f0ca2d6eab9f
AMI <a_yamamoto@innovation.co.jp>
2017-10-25 12:21:52 +0900

    2017/10/25_ボタンの動作を作った

--- myCalc2/PercentViewController.swift
+++ myCalc2/PercentViewController.swift
@@ -20,6 +20,78 @@ class PercentViewController: UIViewController {
             percentFeald.text = "\(percent)"
         }
     }
+    //宿題：0~9と、Cの動作を記入する
+    
+    @IBAction func tap2Button(_ sender: Any) {
+        let value:String = percentFeald.text! + "2"
+        if let percent = Int(value){
+            percentFeald.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap3Button(_ sender: Any) {
+        let value:String = percentFeald.text! + "3"
+        if let percent = Int(value){
+            percentFeald.text = "\(percent)"
+        }
+    }
+    @IBAction func tap4Button(_ sender: Any) {
+        let value:String = percentFeald.text! + "4"
+        if let percent = Int(value){
+            percentFeald.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap5Button(_ sender: Any) {
+        let value:String = percentFeald.text! + "5"
+        if let percent = Int(value){
+            percentFeald.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap6Button(_ sender: Any) {
+        let value:String = percentFeald.text! + "6"
+        if let percent = Int(value){
+            percentFeald.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap7Button(_ sender: Any) {
+        let value:String = percentFeald.text! + "7"
+        if let percent = Int(value){
+            percentFeald.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap8Button(_ sender: Any) {
+        let value:String = percentFeald.text! + "8"
+        if let percent = Int(value){
+            percentFeald.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap9Button(_ sender: Any) {
+        let value:String = percentFeald.text! + "9"
+        if let percent = Int(value){
+            percentFeald.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tap0Button(_ sender: Any) {
+        let value:String = percentFeald.text! + "0"
+        if let percent = Int(value){
+            percentFeald.text = "\(percent)"
+        }
+    }
+    
+    @IBAction func tapCButton(_ sender: Any) {
+        percentFeald.text = "0"
+        
+    }
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
